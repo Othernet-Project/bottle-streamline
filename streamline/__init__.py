82e6d5e6040370ae9916e1d3de30bea20a71d6ea
Branko Vukelic <branko@outernet.is>
2016-04-03 19:12:38 +0200

    Expose template route handlers via package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- streamline/__init__.py
+++ streamline/__init__.py
@@ -1,8 +1,12 @@
 from .base import RouteBase
+from .template import TemplateRoute, XHRPartialRoute, ROCARoute
 
 
 __version__ = '1.0.dev1'
 __author__ = 'Outernet Inc'
 __all__ = (
     RouteBase,
+    TemplateRoute,
+    XHRPartialRoute,
+    ROCARoute,
 )
