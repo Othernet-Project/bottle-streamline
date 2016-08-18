9a94e9e61a7bb1680265692eb7cdf926842aa766
Branko Vukelic <branko@outernet.is>
2016-08-18 22:24:30 +0200

    Fix __all__ using objects instead of strings
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- streamline/__init__.py
+++ streamline/__init__.py
@@ -6,12 +6,12 @@ from .forms import FormRoute, TemplateFormRoute, XHRPartialFormRoute
 __version__ = '1.0'
 __author__ = 'Outernet Inc'
 __all__ = (
-    RouteBase,
-    NonIterableRouteBase,
-    TemplateRoute,
-    XHRPartialRoute,
-    ROCARoute,
-    FormRoute,
-    TemplateFormRoute,
-    XHRPartialFormRoute,
+    'RouteBase',
+    'NonIterableRouteBase',
+    'TemplateRoute',
+    'XHRPartialRoute',
+    'ROCARoute',
+    'FormRoute',
+    'TemplateFormRoute',
+    'XHRPartialFormRoute',
 )
