b49218b2b1dabbf5c52e142aa7164bc24de4d644
Branko Vukelic <branko@outernet.is>
2016-08-31 18:04:58 +0200

    Bump hotfix version
    Also add missing imports in streamline package
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- streamline/__init__.py
+++ streamline/__init__.py
@@ -1,11 +1,13 @@
-from .base import Route, RouteBase, NonIterableRouteBase
+from .base import Route, RouteBase, NonIterableRouteBase, before, after
 from .template import TemplateRoute, XHRPartialRoute, ROCARoute
 from .forms import FormRoute, TemplateFormRoute, XHRPartialFormRoute
 
 
-__version__ = '1.0.post1'
+__version__ = '1.0.post2'
 __author__ = 'Outernet Inc'
 __all__ = (
+    'before',
+    'after',
     'Route',
     'RouteBase',
     'NonIterableRouteBase',
