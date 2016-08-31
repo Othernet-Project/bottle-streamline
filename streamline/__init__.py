3aa8ea300d88bbe7e4097302012e527eb251279a
Andrean Franc <andrean.franc@gmail.com>
2016-08-31 19:48:59 +0200

    Bump version

--- streamline/__init__.py
+++ streamline/__init__.py
@@ -3,7 +3,7 @@ from .template import TemplateRoute, XHRPartialRoute, ROCARoute
 from .forms import FormRoute, TemplateFormRoute, XHRPartialFormRoute
 
 
-__version__ = '1.0.post2'
+__version__ = '1.0.post3'
 __author__ = 'Outernet Inc'
 __all__ = (
     'before',
